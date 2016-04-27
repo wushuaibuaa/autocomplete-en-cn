c60ee4e00ab4d7094befc1410c1131b60eb1f696
Shuai Wu <ws.horton@gmail.com>
2016-04-28 06:13:10 +0800

    Delete extra files.
    Delete extra files.

--- spec/autocomplete-en-cn-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AutocompleteEnCnView from '../lib/autocomplete-en-cn-view';
-
-describe('AutocompleteEnCnView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
